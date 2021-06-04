2cc37696a277736bcc1aaf7bff16c93cf90be265
liaismayanti <85346009+liaismayanti@users.noreply.github.com>
2021-06-04 20:30:07 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,51 +10,23 @@ const token = '1897856346:AAFaL-LbvDagiJKaYFRL0ekv_6j3JQBlOm8'
 const bot = new TelegramBot(token, {polling: true});
 
 
-// Main Menu Bot
+// bots
 bot.onText(/\/start/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
+        click /menu to main menu`
     );   
 });
 
-// input requires i and r
-state = 0;
-bot.onText(/\/predict/, (msg) => { 
+bot.onText(/\/menu/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai i|v contohnya 9|9`
-    );  
-    state = 1;
+        `this is your main menu`
+    );   
 });
-
-bot.on('message'), (msg) => {
-    if(state == 1){
-        s = msg.text.split("|");
-        i = s[0]
-        v = s[1]
-        model.predict(
-            [
-                parseFloat(s[0]), // string to float
-                parseFloat(s[1]),
-            ]
-        ).then((jres)=>{
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai v yang diprediksi adalah $(jres[0]} volt`
-                );
-                bot.sendMessage(
-                    msg.chat.id,
-                    `nilai p yang diprediksi adalah $(jres[1]} volt`
-                );
-        })
-    }else{
-        state = 0
-    }
-})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
